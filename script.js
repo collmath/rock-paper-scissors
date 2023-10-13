cab9c80f0e37cfd57cceef539d9a3d9e648ae2ac
Mathew Colling <144559030+collmath@users.noreply.github.com>
2023-10-12 18:38:40 -0700

    Finished game added function to loop through number of rounds user wants to play

--- script.js
+++ script.js
@@ -11,33 +11,81 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "rock"){
-        return "It's a Tie! you both picked rock";
-    }else if (playerSelection === "rock" && computerSelection === "paper"){
-        return "You Lose! Paper beats rock";
-    }else if (playerSelection === "rock" && computerSelection === "scissors"){
-        return "You Win! rock beats scissors";
-    }else if (playerSelection === "paper" && computerSelection === "paper"){
-        return "It's a Tie! you both picked paper";
-    }else if (playerSelection === "paper" && computerSelection === "scissors"){
-        return "You Lose! scissors beats paper";
-    }else if (playerSelection === "paper" && computerSelection === "rock"){
-        return "You Win! paper beats rock";
-    }else if (playerSelection === "scissors" && computerSelection === "scissors"){
-        return "It's a Tie! you both picked scissors";
-    }else if (playerSelection === "scissors" && computerSelection === "rock"){
-        return "You Lose! Rock beats scissors";
-    }else if (playerSelection === "scissors" && computerSelection === "paper"){
-        return "You Win! scissors beats paper";
+function playRound() {
+    let playerSelection = prompt("Choose Rock, Paper, or Scissors");
+    let computerSelection = getComputerChoice();
+
+    if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock"){
+        console.log("It's a Tie! you both picked rock");
+        return "tie"
+    }else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper"){
+        console.log("You Lose! Paper beats rock");
+        return "lose"
+    }else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors"){
+        console.log("You Win! rock beats scissors");
+        return "win"
+    }else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper"){
+        console.log("It's a Tie! you both picked paper");
+        return "tie"
+    }else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors"){
+        console.log("You Lose! scissors beats paper");
+        return "lose"
+    }else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock"){
+        console.log("You Win! paper beats rock");
+        return "win"
+    }else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors"){
+        console.log("It's a Tie! you both picked scissors");
+        return "tie"
+    }else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock"){
+        console.log("You Lose! Rock beats scissors");
+        return "lose"
+    }else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper"){
+        console.log("You Win! scissors beats paper");
+        return "win"
     }else{
-        return "Something went wrong!?!?"
+        console.log("Something went wrong, did you make a correct selection?")
     }
 }
 
-const computerSelection = getComputerChoice();
-let playerSelection = prompt("someting")
+function ask(){
+    let x = prompt("How many rounds should we play?");
+    return x;
+}
 
-console.log(playerSelection)
 
-console.log(playRound(playerSelection.toLowerCase(), computerSelection))
+
+function game(){
+    let rounds = ask();
+    let wins = 0;
+    let losses = 0;
+    let ties = 0;
+    for (let index = 0; index < rounds; index++) {
+        let play = playRound();
+        switch (play) {
+            case "win":
+                wins++;
+                break;
+            case "lose":
+                losses++;
+                break;
+            case "tie":
+                ties++;
+                break;
+        }
+    }
+    console.log("wins " + String(wins) + " Losses " + String(losses) + " Ties " + String(ties))
+    if (wins > losses){
+        console.log("You won!");
+    }else if (losses > wins){
+        console.log("You lost!");
+    }else{
+        console.log("Its a tie!")
+    }
+    
+}
+    
+
+
+
+
+
